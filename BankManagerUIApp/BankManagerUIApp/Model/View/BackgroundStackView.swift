c9574beb6e2f78969decff2224a3eed478e5710b
yim2627 <yim2627@gmail.com>
2021-12-31 17:21:43 +0900

    feat: CustomerListScrollView, CustomerListStackView 구현 #12
    - CustomerStatusStackView 재사용을 위해 이니셜라이저 구현
    - StatusLabel 구현

--- BankManagerUIApp/BankManagerUIApp/Model/View/BackgroundStackView.swift
+++ BankManagerUIApp/BankManagerUIApp/Model/View/BackgroundStackView.swift
@@ -3,23 +3,23 @@ import UIKit
 class BackgroundStackView: UIStackView {
     override init(frame: CGRect) {
         super.init(frame: frame)
-        configueUI()
+        configUI()
     }
     
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    func configueUI() {
-        let waitingStatusStackView = CustomerStatusStackView()
+    func configUI() {
+        let waitingStatusStackView = CustomerStatusStackView(title: "대기중", bgColor: .systemGreen)
         waitingStatusStackView.axis = .vertical
-        waitingStatusStackView.distribution = .fillEqually
+        waitingStatusStackView.distribution = .fill
         addArrangedSubview(waitingStatusStackView)
         waitingStatusStackView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor)
         
-        let workingStatusStackView = CustomerStatusStackView()
+        let workingStatusStackView = CustomerStatusStackView(title: "업무중", bgColor: .systemBlue)
         workingStatusStackView.axis = .vertical
-        workingStatusStackView.distribution = .fillEqually
+        workingStatusStackView.distribution = .fill
         addArrangedSubview(workingStatusStackView)
         workingStatusStackView.anchor(top: topAnchor, left: waitingStatusStackView.rightAnchor, bottom: bottomAnchor)
     }
--- BankManagerUIApp/BankManagerUIApp/Model/View/CustomerStatusStackView.swift
+++ BankManagerUIApp/BankManagerUIApp/Model/View/CustomerStatusStackView.swift
@@ -1,13 +1,38 @@
 import UIKit
 
 class CustomerStatusStackView: UIStackView {
+    private var statusLabel = UILabel()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
     
     required init(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        fatalError("init(coder:) hastitlenot been implemented")
     }
     
+    convenience init(title: String, bgColor: UIColor) {
+        self.init()
+        configUI()
+        statusLabel.text = title
+        statusLabel.backgroundColor = bgColor
+    }
+    
+    func configUI() {
+        statusLabel.font = .preferredFont(forTextStyle: .title1)
+        statusLabel.textAlignment = .center
+        statusLabel.textColor = .white
+        addArrangedSubview(statusLabel)
+        
+        let customerListScrollView = UIScrollView()
+        addArrangedSubview(customerListScrollView)
+        customerListScrollView.anchor(top: statusLabel.bottomAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor)
+        
+        let customerListStackView = UIStackView()
+        customerListStackView.axis = .vertical
+        customerListStackView.distribution = .fill
+        customerListScrollView.addSubview(customerListStackView)
+        customerListStackView.anchor(top: customerListScrollView.topAnchor, left: customerListScrollView.leftAnchor, bottom: customerListScrollView.bottomAnchor, right: customerListScrollView.rightAnchor)
+    }
     
 }
